de6aae0c254cd69f653c7a1041fd6d55ed9e828f
BrenoZacharias <brenomzacharias@gmail.com>
2021-10-18 19:41:23 -0300

    comeco

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Projeto_Loja_Sapatos.Models;
+
+
+namespace Projeto_Loja_Sapatos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Nome)
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Endereco)
+                .HasMaxLength(200);
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.CNPJ)
+                .HasMaxLength(14)
+                .IsRequired();
+
+            modelBuilder.Entity<Fornecedor>()
+                .Property(p => p.Id);
+
+        }
+    }
+}
---
+++ Models/Fornecedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Loja_Sapatos.Models
+{
+    public class Fornecedor
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string CNPJ { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
